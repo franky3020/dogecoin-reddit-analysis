5da91ad221484f93d9ffb8386501cdff26bb32e9
franky3020 <210238hwsh@gmail.com>
2021-06-05 22:12:07 +0800

    add new

--- nodejs/app.js
+++ nodejs/app.js
@@ -4,10 +4,20 @@ const app = express();
 const port = 3000;
 
 let countSubscribers = -1;
+let active_user_count = -1;
 
 app.get('/metrics', (req, res) => {
-  if(countSubscribers !== -1) {
-    res.send('subscribers ' + countSubscribers);
+
+  let sentMsg = "";
+  let subscribersMsg = "";
+  let active_user_count_Msg = "";
+
+  subscribersMsg = 'subscribers ' + countSubscribers;
+  active_user_count_Msg = 'active_user_count ' + active_user_count;
+
+  if(countSubscribers !== -1 && active_user_count !== -1) {
+    sentMsg = subscribersMsg + '\n' + active_user_count_Msg;
+    res.send(sentMsg);
   }
 });
 
@@ -19,8 +29,15 @@ setInterval(function() {
       return console.log(err);
     }
 
-    if( body.data !== undefined && body.data.subscribers !== undefined) {
-      countSubscribers = body.data.subscribers;
+    if( body.data !== undefined ) {
+      if (body.data.subscribers !== undefined) {
+        countSubscribers = body.data.subscribers;
+      }
+
+      if (body.data.active_user_count !== undefined) {
+        active_user_count = body.data.active_user_count;
+      }
+      
     } else {
       console.log(" body.data.subscribers === undefined");
     }
